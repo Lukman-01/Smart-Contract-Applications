d1d3dd4f746838b559a918f6abd12c0f2ee06772
Lukman-01 <abdulyekeenlukman@gmail.com>
2023-07-20 11:32:39 +0100

    state variables decleared

---
+++ Hotel Booking/contracts/Hotel-Booking.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.5.16;
+
+contract Hotel{
+    address landlord;
+    address tenant;
+
+    uint no-of-rooms;
+    uint no-of-rent;
+    uint no-of-agreement;
+
+    
+}
