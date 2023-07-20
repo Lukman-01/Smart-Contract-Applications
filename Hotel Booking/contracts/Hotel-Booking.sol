236a33b16672e828bccc0130882fbf735f8b1e2f
Lukman-01 <abdulyekeenlukman@gmail.com>
2023-07-20 11:42:06 +0100

    Rooms created

--- Hotel Booking/contracts/Hotel-Booking.sol
+++ Hotel Booking/contracts/Hotel-Booking.sol
@@ -1,14 +1,28 @@
 // SPDX-License-Identifier: MIT
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity ^0.5.16;
+pragma solidity ^0.8.9;
 
 contract Hotel{
-    address landlord;
-    address tenant;
+    address payable landlord;
+    address payable tenant;
 
-    uint no-of-rooms;
-    uint no-of-rent;
-    uint no-of-agreement;
+    uint no_of_rooms = 0;
+    uint no_of_rent = 0;
+    uint no_of_agreement = 0;
+
+    struct Room{
+        uint room_id;
+        uint agreement_id;
+        string room_name;
+        string room_address;
+        uint rent_per_month;
+        uint security_deposit;
+        uint timestamp;
+        bool vacant;
+        address payable landlord;
+        address payable current_tenant;
+    }
+
+    mapping(uint => Room) public Rooms;
 
     
 }
