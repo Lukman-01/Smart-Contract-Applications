8f8c339d9bbd5c4e17f10e39ca433f7e130c0970
Lukman-01 <abdulyekeenlukman@gmail.com>
2023-08-07 12:16:06 +0100

    property struct

--- Real-Estate/contracts/Real-Estate-Contract.sol
+++ Real-Estate/contracts/Real-Estate-Contract.sol
@@ -1,6 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
 contract RealEstate {
-     
+    using SafeMath for uint256;
+
+    struct Property{
+        uint price;
+        address owner;
+        bool forSale;
+        string name;
+        string description;
+        string location;
+    }
+
+    
 }
