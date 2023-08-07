b5ad76db99201a6675755eb9485fac552b1f5787
Lukman-01 <abdulyekeenlukman@gmail.com>
2023-08-07 14:50:48 +0100

    Documented

--- Real-Estate/contracts/Real-Estate-Contract.sol
+++ Real-Estate/contracts/Real-Estate-Contract.sol
@@ -3,6 +3,10 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
+/**
+ * @title RealEstate
+ * @dev A smart contract for managing real estate properties on the Ethereum blockchain.
+ */
 contract RealEstate {
     using SafeMath for uint256;
 
@@ -20,6 +24,14 @@ contract RealEstate {
 
     event PropertySold(uint propertyId);
 
+    /**
+     * @dev List a new property for sale.
+     * @param _id The unique identifier of the property.
+     * @param _price The price of the property in Wei (smallest unit of Ether).
+     * @param _name The name of the property.
+     * @param _description The description of the property.
+     * @param _location The location of the property.
+     */
     function listPropertyForSale(
         uint _id,
         uint _price,
@@ -40,6 +52,10 @@ contract RealEstate {
         propertyIds.push(_id);
     }
 
+    /**
+     * @dev Buy a property listed for sale.
+     * @param _id The identifier of the property to be bought.
+     */
     function buyProperty(uint _id) public payable {
         Property storage property = properties[_id];
 
