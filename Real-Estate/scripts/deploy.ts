78a118f341607ea6122ed2eaea854541e21f201b
Lukman-01 <abdulyekeenlukman@gmail.com>
2023-08-08 16:10:35 +0100

    deploy script added

--- Real-Estate/scripts/deploy.ts
+++ Real-Estate/scripts/deploy.ts
@@ -1,27 +1,21 @@
 import { ethers } from "hardhat";
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+async function deployRealEstate() {
+  const [deployer] = await ethers.getSigners();
 
-  const lockedAmount = ethers.parseEther("0.001");
+  console.log("Deploying RealEstate contract with the account:", deployer.address);
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  const RealEstate = await ethers.getContractFactory("RealEstate");
+  const realEstateContract = await RealEstate.deploy();
+  await realEstateContract.deployed();
 
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log("RealEstate contract address:", realEstateContract.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+// Run the deployment function
+deployRealEstate()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
