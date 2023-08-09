a6b2e6f574b0915faa5c4075061dfbd1229e8934
Lukman-01 <abdulyekeenlukman@gmail.com>
2023-08-09 20:55:30 +0100

    hardhat config

--- Real-Estate/hardhat.config.ts
+++ Real-Estate/hardhat.config.ts
@@ -3,6 +3,12 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    goerli: {
+      url: 'https://eth-goerli.g.alchemy.com/v2/PtOW84EIMyfAaX93sYECGs_QLonqicN8',
+      accounts: ['8431b27ab2ba78ecc97181e3baf0defe507b490a78ecbb7b25f0e4621278d66f'],
+    },
+  },
 };
 
 export default config;
