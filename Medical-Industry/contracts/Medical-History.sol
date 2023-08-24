ea80f663e3f0e188e80bb7375cf3eda4b50b6b7f
Lukman-01 <abdulyekeenlukman@gmail.com>
2023-08-24 12:40:24 +0100

    update patient func

--- Medical-Industry/contracts/Medical-History.sol
+++ Medical-Industry/contracts/Medical-History.sol
@@ -31,4 +31,22 @@ contract MedicalHistory {
             procedures: _procedures
         });
     }
+
+    function updatePatient(
+        string memory _name,
+        uint _age,
+        string[] memory _conditions,
+        string[] memory _allergies,
+        string[] memory _medications,
+        string[] memory _procedures
+    ) public {
+        Patient storage patient = Patients[msg.sender];
+        patient.name = _name;
+        patient.age = _age;
+        patient.conditions = _conditions;
+        patient.allergies = _allergies;
+        patient.medications = _medications;
+        patient.procedures = _procedures;
+    }
+    
 }
