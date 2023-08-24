0990f90cf0ca8b408082d10818252e2a369af265
Lukman-01 <abdulyekeenlukman@gmail.com>
2023-08-24 12:38:38 +0100

    addPatient func

--- Medical-Industry/contracts/Medical-History.sol
+++ Medical-Industry/contracts/Medical-History.sol
@@ -7,12 +7,28 @@ contract MedicalHistory {
         string name;
         uint age;
         string[] conditions;
-        string[] alergies;
+        string[] allergies;
         string[] medications;
-        string procedures;
+        string[] procedures;
     }
 
     mapping(address => Patient) public Patients;
 
-
+    function addPatient(
+        string memory _name,
+        uint _age,
+        string[] memory _conditions,
+        string[] memory _allergies,
+        string[] memory _medications,
+        string[] memory _procedures
+    ) public {
+        Patients[msg.sender] = Patient({
+            name: _name,
+            age: _age,
+            conditions: _conditions,
+            allergies: _allergies,
+            medications: _medications,
+            procedures: _procedures
+        });
+    }
 }
