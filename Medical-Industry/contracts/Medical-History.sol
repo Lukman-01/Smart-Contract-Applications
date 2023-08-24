2bdfd3579b25790aebbd6db719de72f32087df41
Lukman-01 <abdulyekeenlukman@gmail.com>
2023-08-24 12:44:58 +0100

    added functionalities

--- Medical-Industry/contracts/Medical-History.sol
+++ Medical-Industry/contracts/Medical-History.sol
@@ -2,60 +2,77 @@
 pragma solidity ^0.8.9;
 
 contract MedicalHistory {
-     
-    struct Patient{
+    struct Patient {
         string name;
         uint age;
         string[] conditions;
         string[] allergies;
         string[] medications;
         string[] procedures;
+        uint256 lastUpdated; // Timestamp of the last update
     }
 
     mapping(address => Patient) public Patients;
+    mapping(address => bool) public authorizedDoctors;
+
+    modifier onlyAuthorizedDoctor() {
+        require(authorizedDoctors[msg.sender], "Unauthorized doctor");
+        _;
+    }
+
+    constructor(address[] memory _doctors) {
+        for (uint256 i = 0; i < _doctors.length; i++) {
+            authorizedDoctors[_doctors[i]] = true;
+        }
+    }
 
     function addPatient(
+        address _patientAddress,
         string memory _name,
         uint _age,
         string[] memory _conditions,
         string[] memory _allergies,
         string[] memory _medications,
         string[] memory _procedures
-    ) public {
-        Patients[msg.sender] = Patient({
+    ) public onlyAuthorizedDoctor {
+        Patients[_patientAddress] = Patient({
             name: _name,
             age: _age,
             conditions: _conditions,
             allergies: _allergies,
             medications: _medications,
-            procedures: _procedures
+            procedures: _procedures,
+            lastUpdated: block.timestamp
         });
     }
 
     function updatePatient(
+        address _patientAddress,
         string memory _name,
         uint _age,
         string[] memory _conditions,
         string[] memory _allergies,
         string[] memory _medications,
         string[] memory _procedures
-    ) public {
-        Patient storage patient = Patients[msg.sender];
+    ) public onlyAuthorizedDoctor {
+        Patient storage patient = Patients[_patientAddress];
         patient.name = _name;
         patient.age = _age;
         patient.conditions = _conditions;
         patient.allergies = _allergies;
         patient.medications = _medications;
         patient.procedures = _procedures;
+        patient.lastUpdated = block.timestamp;
     }
-    
+
     function getPatient(address _patientAddress) public view returns (
         string memory _name,
         uint _age,
         string[] memory _conditions,
         string[] memory _allergies,
         string[] memory _medications,
-        string[] memory _procedures
+        string[] memory _procedures,
+        uint256 _lastUpdated
     ) {
         Patient storage patient = Patients[_patientAddress];
         return (
@@ -64,7 +81,8 @@ contract MedicalHistory {
             patient.conditions,
             patient.allergies,
             patient.medications,
-            patient.procedures
+            patient.procedures,
+            patient.lastUpdated
         );
     }
 }
