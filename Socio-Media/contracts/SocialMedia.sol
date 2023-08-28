ce5c03d066e484e62a5b22bf19fd31447a94f801
Lukman-01 <abdulyekeenlukman@gmail.com>
2023-08-28 08:33:36 +0100

    updated to V2

--- Socio-Media/contracts/SocialMedia.sol
+++ Socio-Media/contracts/SocialMedia.sol
@@ -1,65 +1,96 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract SocioMedia {
-    struct  User {
-    string name;
-    string bio;
-    address[] friends;
-    Message[] messages;
+contract ModernSocialMedia {
+    struct User {
+        string name;
+        string bio;
+        string profilePicture;
+        address[] friends;
+        Post[] posts;
+        Message[] messages;
+        mapping(address => bool) followers;
+        mapping(uint256 => Comment[]) postComments;
     }
 
-    mapping (address => User) public users;
-    struct Message{
-        address sender;
-        address recipient;
+    struct Post {
+        address author;
         string content;
-        uint timestamp;
+        uint256 timestamp;
+        uint256 likes;
     }
 
-    Message[] public messages;
-
-    function createUser(string memory _name, string memory _bio) public{
-        User memory newUser = User(_name, _bio, new address[](0), new Message[](0));
-        users[msg.sender] = newUser;
+    struct Comment {
+        address author;
+        string content;
+        uint256 timestamp;
     }
 
-    function makeFriend(address _friend) public{
-        require (users[_friend].name != "", "User does not exist");
-        require (!isFriend[_friend], "Friend already exist");
+    mapping(address => User) public users;
 
-        users[msg.sender].friends.push(_friend);
-        users[_friend].friends.push(msg.sender);
+    event PostCreated(address indexed author, uint256 indexed postId);
+    event CommentAdded(address indexed author, uint256 indexed postId, uint256 commentId);
+    event UserFollowed(address indexed follower, address indexed following);
+    event PostLiked(address indexed liker, address indexed author, uint256 indexed postId);
+    event ProfileUpdated(address indexed user, string newName, string newBio, string newProfilePicture);
+    event MessageSent(address indexed sender, address indexed recipient, string content);
+
+    modifier onlyExistingUser() {
+        require(bytes(users[msg.sender].name).length > 0, "User does not exist");
+        _;
     }
 
-    function deleteAcount() public{
-        delete users[msg.sender];
+    function createUser(string memory _name, string memory _bio, string memory _profilePicture) public {
+        require(bytes(_name).length > 0, "Name cannot be empty");
+        users[msg.sender] = User(_name, _bio, _profilePicture, new address[](0), new Post[](0), new Message[](0));
     }
 
-    function updateAccount(string memory _name, string memory _bio) public{
-        users[msg.sender].name = _name;
-        users[msg.sender].bio = _bio;  
+    function createPost(string memory _content) public onlyExistingUser {
+        Post memory newPost = Post(msg.sender, _content, block.timestamp, 0);
+        users[msg.sender].posts.push(newPost);
+        emit PostCreated(msg.sender, users[msg.sender].posts.length - 1);
     }
 
-    function sendmessage(address _recipient, string memory _content) public{
-        require(users[_recipient].name != "", "Recipient doesnot exist");
+    function addComment(uint256 _postId, string memory _content) public onlyExistingUser {
+        require(_postId < users[msg.sender].posts.length, "Invalid post ID");
+        Comment memory newComment = Comment(msg.sender, _content, block.timestamp);
+        users[msg.sender].postComments[_postId].push(newComment);
+        emit CommentAdded(msg.sender, _postId, users[msg.sender].postComments[_postId].length - 1);
+    }
+
+    function likePost(address _author, uint256 _postId) public onlyExistingUser {
+        require(users[_author].posts.length > _postId, "Invalid post ID");
+        users[_author].posts[_postId].likes++;
+        emit PostLiked(msg.sender, _author, _postId);
+    }
+
+    function followUser(address _userToFollow) public onlyExistingUser {
+        require(_userToFollow != msg.sender && bytes(users[_userToFollow].name).length > 0, "Invalid user");
+        users[msg.sender].following[_userToFollow] = true;
+        users[_userToFollow].followers[msg.sender] = true;
+        emit UserFollowed(msg.sender, _userToFollow);
+    }
+
+    function unfollowUser(address _userToUnfollow) public onlyExistingUser {
+        require(users[_userToUnfollow].followers[msg.sender], "You are not following this user");
+        users[msg.sender].following[_userToUnfollow] = false;
+        users[_userToUnfollow].followers[msg.sender] = false;
+    }
+
+    function updateProfile(string memory _newName, string memory _newBio, string memory _newProfilePicture) public onlyExistingUser {
+        User storage user = users[msg.sender];
+        user.name = _newName;
+        user.bio = _newBio;
+        user.profilePicture = _newProfilePicture;
+        emit ProfileUpdated(msg.sender, _newName, _newBio, _newProfilePicture);
+    }
+
+    function sendMessage(address _recipient, string memory _content) public onlyExistingUser {
+        require(bytes(users[_recipient].name).length > 0, "Recipient does not exist");
 
         Message memory newMessage = Message(msg.sender, _recipient, _content, block.timestamp);
         users[msg.sender].messages.push(newMessage);
         users[_recipient].messages.push(newMessage);
-        messages.push(newMessage);
+        emit MessageSent(msg.sender, _recipient, _content);
     }
-
-    function getMessages() public view returns(Message[] memory){
-        return users[msg.sender].messages;
-    }
-
-    function getAllUsers() public view returns(address[] memory) {
-    address[] memory allUsers = new address[](users.length);
-    for (uint i = 0; i < allUsers.length; i++) {
-        allUsers[i] = address(users[i]);
-    }
-    return allUsers;
-}
-
 }
