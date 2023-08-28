cbcd6aba47a8ebf477792f0dd2020ac9e7e71b8e
Lukman-01 <abdulyekeenlukman@gmail.com>
2023-08-28 07:29:23 +0100

    user and message struct

--- Socio-Media/contracts/SocialMedia.sol
+++ Socio-Media/contracts/SocialMedia.sol
@@ -2,5 +2,22 @@
 pragma solidity ^0.8.9;
 
 contract SocioMedia {
-     
+    struct  User {
+    string name;
+    string bio;
+    address[] friends;
+    Message[] messages;
+    }
+
+    mapping ((address) => User) public users;
+    struct Message{
+        address sender;
+        address recipient;
+        string content;
+        uint timestamp;
+    }
+
+    Message[] public messages;
+
+    
 }
