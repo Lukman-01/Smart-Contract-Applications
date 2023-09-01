6d0c4c726591fcea0e43f5372b7cffb42b0cd373
Lukman-01 <abdulyekeenlukman@gmail.com>
2023-09-01 05:44:25 +0100

    candidate functions

--- Voting-Contract/contracts/Voting.sol
+++ Voting-Contract/contracts/Voting.sol
@@ -60,5 +60,62 @@ contract Lock {
         string voter_ipfs
     );
 
-    
+    constructor (){
+        votingOrganizer = msg.sender;
+    }
+
+    function setCandidate(address _add, string memory _name, string memory _age, string memory _image, string memory _ipfs) public {
+        require(votingOrganizer == msg.sender, "Only Organizer can authorize candidate");
+
+        _candidateId.increment();
+        uint idNumber = _candidateId.current();
+
+        Candidate storage candidate = candidates[_add];
+        candidate.candidateId = idNumber;
+        candidate.name = _name;
+        candidate.age = _age;
+        candidate.image = _image;
+        candidate.voteCount = 0;
+        candidate._address = _add;
+        candidate.ipfs = _ipfs;
+
+        candidateAddress.push(_add);
+
+        emit CandidateCreated(
+            idNumber,
+            _name,
+            _age,
+            _image,
+            0,
+            _add,
+            _ipfs
+        );
+    }
+
+    function getCandidateAddresses() public view returns (address[] memory) {
+        return candidateAddress;
+    }
+
+    function getCandidateData(address _candidateAddress) public view returns (
+        string memory name,
+        string memory age,
+        string memory image,
+        uint voteCount,
+        string memory ipfs
+    ) {
+        Candidate memory candidate = candidates[_candidateAddress];
+        return (
+            candidate.name,
+            candidate.age,
+            candidate.image,
+            candidate.voteCount,
+            candidate.ipfs
+        );
+    }
+
+    function getCandidateLength() public view returns (uint) {
+        return candidateAddress.length;
+    }
+
+
 }
