bcec1f810237447ca3b7cdae9aee4caaea578dda
Lukman-01 <abdulyekeenlukman@gmail.com>
2024-08-21 22:16:57 +0100

    secure and optimized

--- Other Contracts/contracts/Lottery.sol
+++ Other Contracts/contracts/Lottery.sol
@@ -2,39 +2,61 @@
 
 pragma solidity ^0.8.0;
 
-contract Lottery{
- //entities - manager,players and winner
- address public manager;
- address payable[] public players;
- address payable public winner;
+contract Lottery {
+    // Entities: manager, players, and winner
+    address public manager;
+    address payable[] public players;
+    address payable public winner;
 
-  constructor(){
-      manager=msg.sender;
-  }
+    // Reentrancy guard variable
+    bool private locked = false;
 
-  function participate() public payable{
-      require(msg.value==1 ether,"Please pay 1 ether only");
-      players.push(payable(msg.sender));
-  }
+    event PlayerParticipated(address indexed player);
+    event WinnerPicked(address indexed winner, uint256 amount);
 
-  function getBalance() public view returns(uint){
-      require(manager==msg.sender,"You are not the manager");
-      return address(this).balance;
-  }
+    modifier onlyManager() {
+        require(msg.sender == manager, "You are not the manager");
+        _;
+    }
 
-  function random() internal view returns(uint){
-      return uint(keccak256(abi.encodePacked(block.prevrandao,block.timestamp,players.length)));
-  }
+    modifier noReentrant() {
+        require(!locked, "ReentrancyGuard: reentrant call");
+        locked = true;
+        _;
+        locked = false;
+    }
 
-  function pickWinner() public{
-      require(manager==msg.sender,"You are not the manager");
-      require(players.length>=3,"Players are less than 3");
+    constructor() {
+        manager = msg.sender;
+    }
 
-      uint r=random();
-      uint index = r%players.length;
-      winner=players[index];
-      winner.transfer(getBalance());
-      players= new address payable[](0); //this will intiliaze the players array back to 0
-  }
+    function participate() public payable {
+        require(msg.value == 1 ether, "Please pay exactly 1 ether to participate");
+        players.push(payable(msg.sender));
+        emit PlayerParticipated(msg.sender);
+    }
 
+    function getBalance() public view onlyManager returns (uint256) {
+        return address(this).balance;
+    }
+
+    function random() internal view returns (uint256) {
+        // Using keccak256 with block prevrandao, timestamp, and player length to generate randomness
+        return uint256(keccak256(abi.encodePacked(block.prevrandao, block.timestamp, players.length)));
+    }
+
+    function pickWinner() public onlyManager noReentrant {
+        require(players.length >= 3, "Players are less than 3");
+
+        uint256 r = random();
+        uint256 index = r % players.length;
+        winner = players[index];
+
+        // Transfer balance to the winner
+        winner.transfer(address(this).balance);
+        emit WinnerPicked(winner, address(this).balance);
+
+        // Reset the players array
+        delete players;
+    }
 }
